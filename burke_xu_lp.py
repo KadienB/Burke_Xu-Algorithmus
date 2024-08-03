e904693127520d61dc559209b93eed1e55077c0b
basti <bastian.baumann@stud-mail.uni-wuerzburg.de>
2024-08-03 15:08:20 +0000

    not working

--- burke_xu_lp.py
+++ burke_xu_lp.py
@@ -83,18 +83,25 @@ def burke_xu_lp(
     S2 = None
 
     # Startwerte der Iterationsvariablen
-    x = 0
-    l = 0
-    s = 0
-    mu = 0
+    x = np.linalg.inv(A) @ b
+    l = np.zeros(A.shape[1])
+    s = c
+    if verbose:
+        print(f"Ax - b = {A @ x - b}")
+        print(f"A^T*lambda + s - c = {A.T @ l + s - c}")
+
+    mu = 1000000
     if np.linalg.norm(mt.big_phi(x, s, 0)) < acc:
+        print(f"initval was solution")
         maxiter = 0
 
     # Festsetzung der Stellschrauben
-    beta = 0
-    sigma = 0
-    alpha1 = 0
-    alpha2 = 0
+    beta = 1000
+    while np.linalg.norm(mt.big_phi(x, s, mu, verbose=verbose)) > beta * mu:
+        beta = beta * 2
+    sigma = 0.5
+    alpha_1 = 0.9
+    alpha_2 = 0.99
 
 
     """ Ausführung des Algorithmus """
@@ -106,19 +113,107 @@ def burke_xu_lp(
 
     # Schleife zur Ausführung des Algorithmus
     for k in range(maxiter):
+
         # Ausgabe der aktuellen Iterationsvariablen
         print(f"-----------------------------------------------------------")
         print(f"x^{k} = {x}")
+        print(f"lambda^{k} = {l}")
         print(f"s^{k} = {s}")
         print(f"mu_{k} = {mu}")
         print(f"-----------------------------------------------------------")
+
         # Prädiktor-Schritt
+        lhs = mt.linear_equation_formulate_lhs(x, s, l, mu, A, problem=1, verbose=verbose)
+        rhs = mt.linear_equation_formulate_rhs(x, s, l, mu, sigma, A, problem=1, steptype=1, verbose=verbose)
+        cholesky, low = sp.linalg.cho_factor(lhs)
+        delta_l = sp.linalg.cho_solve((cholesky, low), rhs)
+        delta_s = -1 * A.T @ delta_l
+        delta_x = (-1 * np.diag(mt.nabla_big_phi(x, s, mu, 2, inv=True, verbose=verbose)) @ delta_s) + (mt.big_phi(x, s, mu, verbose=verbose)) + (-1 * mu * mt.nabla_big_phi(x, s, mu, 3, verbose=verbose))
+        x, l, s, mu, step = mt.predictor_step(x, s, l, delta_x, delta_s, delta_l, mu, alpha_1, beta, acc, verbose=verbose)
+
         # Korrektor-Schritt
+        if step == 0:
+            if verbose:
+                print("Nullstep has been taken.")
+                print(f"-----------------------------------------------------------")
+                print(f"x^{k} = {x}")
+                print(f"lambda^{k} = {l}")
+                print(f"s^{k} = {s}")
+                print(f"mu_{k} = {mu}")
+                print(f"-----------------------------------------------------------")
+            nullstep += 1
+            rhs = mt.linear_equation_formulate_rhs(x, s, l, mu, sigma, A, problem=1, steptype=2, verbose=verbose)
+            delta_l = sp.linalg.cho_solve((cholesky, low), rhs)
+            delta_s = -1 * A.T @ delta_l
+            delta_x = (-1 * np.diag(mt.nabla_big_phi(x, s, mu, 2, inv=True, verbose=verbose)) @ delta_s) + (mt.big_phi(x, s, mu, verbose)) + (- 1 * mu * sigma * mt.nabla_big_phi(x, s, mu, 3, verbose=verbose))
+        elif step == 1:
+            print(f"-----------------------------------------------------------")
+            print(f"x^{k + 1} = {x}")
+            print(f"s^{k + 1} = {y}")
+            print(f"mu_{k + 1} = {mu}")
+            print(f"-----------------------------------------------------------")
+            maxiter = k + 1
+            break
+        elif step == 2:
+            lhs = mt.linear_equation_formulate_lhs(x, s, l, mu, A, problem=1, verbose=verbose)
+            rhs = mt.linear_equation_formulate_rhs(x, s, l, mu, sigma, A, problem=1, steptype=2, verbose=verbose)
+            cholesky, low = sp.linalg.cho_factor(lhs)
+            delta_l = sp.linalg.cho_solve((cholesky, low), rhs)
+            delta_s = -1 * A.T @ delta_l
+            delta_x = (-1 * np.diag(mt.nabla_big_phi(x, s, mu, 2, inv=True, verbose=verbose)) @ delta_s) + (mt.big_phi(x, s, mu, verbose)) + (- 1 * mu * sigma * mt.nabla_big_phi(x, s, mu, 3, verbose))
+        x, l, s, mu = mt.corrector_step(x, s, l, delta_x, delta_s, delta_l, mu, alpha_2, beta, sigma, verbose=verbose)
 
 
     """ Ausgabe des Ergebnisses """
 
-    # Ausgabe des Ergebnisses
+    end_time = time.time()
+
+    # dim_qp = len(x) - dim_diff
+    # qp_sol = x[:dim_qp]
+    # if scaling == 0:
+    #     qp_sol_y = M @ x + q
+    # else:
+    #     qp_sol_y = M_old @ x + q_old
+    # qp_sol_smooth = np.where(np.abs(qp_sol) < acc, 0, qp_sol)
+    # qp_sol_y_smooth = np.where(np.abs(qp_sol_y) < acc, 0, qp_sol_y)
+
+    # if verbose:
+    #     print(f"Es wurde Skalierungsmethode {scaling} verwendet.")
+    #     print(f"Die Genauigkeit beträgt {acc}.")
+    #     print(f"Die Lösung (x,y) des LCP(q,M) wie in der Thesis beschrieben lautet")
+    #     print(f"x in |R^{len(x)} =")
+    #     print(x)
+    #     print(f"y in |R^{len(y)} =")
+    #     print(qp_sol_y)
+
+    # print(f"x in |R^{dim_qp} =")
+    # print(qp_sol)
+    # print(f"Der vorhergehende Vektor x löst das zugehörige Quadratische Programm, oder im Fall 'A == None' das zugehörige lineare Komplementaritätsproblem.")
+    # print(f"Es wurden dafür {maxiter} Schritte durchgeführt. Es wurden {end_time - start_time} Sekunden benötigt.")
+    # print(f"Es wurde {nullstep} mal der Prediktor-Schritt abgelehnt.")
 
 
-    return x, s
+    """ Überprüfung des Ergebnisses """
+
+    return x
+
+# Koeffizienten der Zielfunktion
+c = np.array([-1, -2])
+
+# Koeffizientenmatrix der Gleichungsnebenbedingungen (linke Seite)
+A_eq = np.array([[1, 1],
+                 [1, 2]])
+
+# Rechte Seite der Gleichungsnebenbedingungen
+b_eq = np.array([2, 3])
+
+# Lösen des linearen Programms
+result = sp.optimize.linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=(0, None), method='highs')
+
+my_result = burke_xu_lp(c = c, A = A_eq, b = b_eq, maxiter=25, acc=1e-8, verbose=True)
+print(f"my_result = {my_result}")
+
+# Ausgabe der Ergebnisse
+print("Lösungsstatus:", result.message)
+print("Optimale Lösung x:", result.x)
+print("Optimaler Zielfunktionswert:", result.fun)
--- methods.py
+++ methods.py
@@ -15,7 +15,7 @@ def big_phi(
     mu: float,
     verbose: bool = False,
 )   -> Optional[np.ndarray]:
-    r"""Verwendet die Chen-Harker-Kanzow Glättungsmethode small_phi um einen den Vektor \Phi(x,y,mu) in |R^n wie in Kapitel 1.2 der Thesis zu erzeugen.
+    r"""Verwendet die Chen-Harker-Kanzow Glättungsmethode small_phi um den Vektor \Phi(x,y,mu) in |R^n wie in Kapitel 1.2 der Thesis zu erzeugen.
 
     Die Berechnung hat dabei Zeilenweise die Form
 
@@ -29,7 +29,7 @@ def big_phi(
     b:
         in der Regel aktueller y-Vektor in |R^n.
     mu:
-        Glättungsparameter.
+        Glättungsparameter mu > 0.
     verbose: bool
         Boolean Variable um eine Ausgabe sämtlicher Zwischenergebnisse zu erzeugen.
     """
@@ -52,6 +52,7 @@ def nabla_big_phi(
     b: np.ndarray,
     mu: float,
     arg: int,
+    inv: Optional[bool] = False,
     verbose: bool = False,
 )   -> Optional[np.ndarray]:
     r"""Berechnung der Teilblockmatrizen von nabla(x,y)F wie in Remark 4. und 5. beschrieben unter der Verwendung von
@@ -72,7 +73,7 @@ def nabla_big_phi(
     b:
         in der Regel aktueller y-Vektor in |R^n.
     mu:
-        Glättungsparameter.
+        Glättungsparameter mu > 0.
     arg:
         Integer, der aussagt nach welchem Argument abgeleitet wird.
     verbose: bool
@@ -88,7 +89,11 @@ def nabla_big_phi(
     if arg == 1:
         solution = 1 - ((a - b) / np.sqrt((a - b) ** 2 + 4 * mu ** 2))
     elif arg == 2:
-        solution = 1 + ((a - b) / np.sqrt((a - b) ** 2 + 4 * mu ** 2))
+        if inv == False:
+            solution = 1 + ((a - b) / np.sqrt((a - b) ** 2 + 4 * mu ** 2))
+        else:
+            sqrt_term = np.sqrt((a**2) - (2 * a * b) + (b**2) + (4 * mu**2))
+            solution = sqrt_term / ((a - b) + sqrt_term)
     elif arg == 3: 
         solution = (-4 * mu) / np.sqrt((a - b) ** 2 + 4 * mu ** 2)
     else: 
@@ -101,47 +106,216 @@ def nabla_big_phi(
 
 def linear_equation_formulate_lhs(
     x: np.ndarray,
-    s: np.ndarray,
-    l: Optional[np.ndarray],
+    a: np.ndarray,
+    b: Optional[np.ndarray],
     mu: float,
-    sigma: float,
     A: Union[np.ndarray, spa.csc_matrix],
-    arg1: int,
-    arg2: int,
+    problem: int,
     verbose: bool = False,    
 )   -> Optional[Tuple[np.ndarray, np.ndarray, np.ndarray]]:
-    r"""Formulierung des zu lösenden LGS unter Berücksichtigung von Remarks 4 und 5 und unter Verwendung des Ansatzes des Schurkomplements für lineare Gleichungssysteme mit Blockmatrizen.
-
-    Es genügt also in jedem Schritt das LGS
-
-    (nabla_x(PHI) + nabla_y(PHI) @ M) @ delta_x = -PHI + mu * (sigma) * nabla_mu(PHI)
-
-    zu lösen.
+    r"""Formulierung der linken Seite des Gleichungssystem, das in jedem Prädiktor- und Korrektorschritt gelöst werden muss.
+    Je nachdem ob es sich um LP, QP oder LCP handelt angepasst, für genauere Herleitung siehe Thesis Kapitel 3.
 
     
     Parameters
     ----------
     x: 
-        in der Regel aktueller x-Vektor in |R^(n+m).
-    s:
-        in der Regel aktueller y-Vektor in |R^(n+m).
-    l:
-        in der Regel aktueller l-Vektor in |R^n.
+        in der Regel aktueller Iterationsvektor x in |R^n.
+    a:
+        in der Regel aktueller Iterationsvektor (s bei LP / y bei LCP) in |R^n.
+    b:
+        wenn vorhande, aktueller Iterationsvektor (lambda bei LP), normalerweise in |R^m.
     mu:
-        Glättungsparameter.
-    sigma:
-        Zentrierungparameter.
+        Glättungsparameter mu > 0.
     A:
-        Matrix M von LCP(q,M) in |R^((n+m)x(n+m)).
+        Matrix (A in LP, M in LCP) von LCP(q,M) in |R^mxn.
     arg:
-        Integer, der aussagt nach welchem Argument abgeleitet wird.
-    arg2:
         Integer, der aussagt nach welchem Argument abgeleitet wird.
     verbose: bool
         Boolean Variable um eine Ausgabe sämtlicher Zwischenergebnisse zu erzeugen.
     """
 
-    return 
+    if problem == 1:
+        # Vorerst instabil
+        D_s_inv = nabla_big_phi(x, a, mu, 2, inv=True, verbose=verbose)
+        # vorerst instabil 
+        D_x = nabla_big_phi(x, a, mu, 1, verbose=verbose)
+        lhs = A @ np.diag(D_s_inv) @ np.diag(D_x) @ A.T
+
+
+    if verbose:
+        print(f"Die Koeffizientenmatrix hat die Form: lhs =")
+        print(f"{lhs}")
+    
+    return lhs
+
+def linear_equation_formulate_rhs(
+    x: np.ndarray,
+    a: np.ndarray,
+    b: Optional[np.ndarray],
+    mu: float,
+    sigma: Optional[float],
+    A: Union[np.ndarray, spa.csc_matrix],
+    problem: int,
+    steptype: int,
+    verbose: bool = False,
+)   -> Optional[Tuple[np.ndarray, np.ndarray, np.ndarray]]:
+    r"""Formulierung der rechten Seite des Gleichungssystem, das in jedem Prädiktor- und Korrektorschritt gelöst werden muss.
+    Je nachdem ob es sich um LP, QP oder LCP handelt angepasst, für genauere Herleitung siehe Thesis Kapitel 3.
+
+    
+    Parameters
+    ----------
+    x: 
+        in der Regel aktueller Iterationsvektor x in |R^n.
+    a:
+        in der Regel aktueller Iterationsvektor (s bei LP / y bei LCP) in |R^n.
+    b:
+        wenn vorhande, aktueller Iterationsvektor (lambda bei LP), normalerweise in |R^m.
+    mu:
+        Glättungsparameter mu > 0.
+    A:
+        Matrix (A in LP, M in LCP) von LCP(q,M) in |R^mxn.
+    arg:
+        Integer, der aussagt nach welchem Argument abgeleitet wird.
+    verbose: bool
+        Boolean Variable um eine Ausgabe sämtlicher Zwischenergebnisse zu erzeugen.
+    """
+
+    if problem == 1:
+        # Vorerst instabil
+        D_s = nabla_big_phi(x, a, mu, 2, verbose)
+        D_s_inv = 1 / D_s
+        # vorerst instabil
+        if steptype == 1:
+            rhs = A @ np.diag(D_s_inv) @ ((big_phi(x, a, mu, verbose)) + (-1 * mu * nabla_big_phi(x, a, mu, 3, verbose)))
+        elif steptype == 2:
+            rhs = A @ np.diag(D_s_inv) @ ((big_phi(x, a, mu, verbose)) + (- 1 * mu * sigma * nabla_big_phi(x, a, mu, 3, verbose)))
+        else:
+            raise ValueError("Steptype must be 1 or 2.")
+
+
+    if verbose:
+        print(f"Die rechte Seite lautet hat die Form: rhs =")
+        print(f"{rhs}")
+    
+    return rhs
+
+def predictor_step(
+    x: np.ndarray,
+    a: np.ndarray,
+    b: Optional[np.ndarray],
+    delta_x: np.ndarray,
+    delta_a: np.ndarray,
+    delta_b: Optional[np.ndarray],
+    mu: float,
+    alpha_1: float,
+    beta: float,
+    acc: float,
+    verbose: bool = False,    
+)   -> Optional[np.ndarray]:
+    r"""Beschreibung
+
+    Beschreibung
+
+    Parameters
+    ----------
+    a: 
+        in der Regel aktueller x-Vektor in |R^n.
+    b:
+        in der Regel aktueller y-Vektor in |R^n.
+    mu:
+        Glättungsparameter.
+    arg:
+        Integer, der aussagt nach welchem Argument abgeleitet wird.
+    verbose: bool
+        Boolean Variable um eine Ausgabe sämtlicher Zwischenergebnisse zu erzeugen.
+    """
+    
+    if verbose:
+        print(f"Starting predictor_step calculation...")
+
+    # if np.linalg.norm(np.minimum(x + delta_x,y + delta_y)) < acc:
+    if np.linalg.norm(big_phi(x + delta_x, a + delta_a, 0)) < acc:
+        step = 1
+        x = x + delta_x
+        a = a + delta_a
+        if b is not None:
+            b = b + delta_b
+    elif np.linalg.norm(big_phi(x + delta_x, a + delta_a, mu, verbose=verbose)) > beta * mu:
+        step = 0
+    else:
+        step = 2
+        s = 1
+        while np.linalg.norm(big_phi(x + delta_x, a + delta_a, (alpha_1 ** s) * mu, verbose=verbose)) <= (alpha_1 ** s) * beta * mu:
+            if verbose:
+                print(f"erhöhe s um 1 auf {s}, {alpha_1 ** s} noch zu groß")
+            s += 1
+        if verbose:
+            print(f"{np.linalg.norm(big_phi(x + delta_x, a + delta_a, (alpha_1 ** (s-1)) * mu, verbose=verbose))} <= {(alpha_1 ** (s-1)) * beta * mu}")
+            print(f"{np.linalg.norm(big_phi(x + delta_x, a + delta_a, (alpha_1 ** (s)) * mu, verbose=verbose))} > {(alpha_1 ** s) * beta * mu}") 
+            print(f"daher eta_k = {alpha_1 ** (s-1)}")
+        x = x + delta_x
+        a = a + delta_a
+        mu = (alpha_1 ** (s-1)) * mu
+        if b is not None:
+            b = b + delta_b
+    
+    if b is not None: 
+        return x, b, a, mu, step
+
+    return x, a, mu, step
+
+def corrector_step(
+    x: np.ndarray,
+    a: np.ndarray,
+    b: Optional[np.ndarray],
+    delta_x: np.ndarray,
+    delta_a: np.ndarray,
+    delta_b: Optional[np.ndarray],
+    mu: float,
+    alpha_2: float,
+    beta: float,
+    sigma: float,
+    verbose: bool = False,    
+)   -> Optional[np.ndarray]:
+    r"""Beschreibung
+
+    Beschreibung
+
+    Parameters
+    ----------
+    a: 
+        in der Regel aktueller x-Vektor in |R^n.
+    b:
+        in der Regel aktueller y-Vektor in |R^n.
+    mu:
+        Glättungsparameter.
+    arg:
+        Integer, der aussagt nach welchem Argument abgeleitet wird.
+    verbose: bool
+        Boolean Variable um eine Ausgabe sämtlicher Zwischenergebnisse zu erzeugen.
+    """
+    
+    if verbose:
+        print(f"Starting corrector_step calculation...")
+
+    t = 0
+    while np.linalg.norm(big_phi(x + ((alpha_2 ** t) * delta_x), a + ((alpha_2 ** t) * delta_a), (1 - (sigma * (alpha_2 ** t))) * mu, verbose=verbose)) > (1 - (sigma * (alpha_2 ** t))) * beta * mu:
+        t += 1
+        if t == 80000:
+            raise ValueError("Corrector-Step kann nicht durchgeführt werden. Run-Time-Error 't' > 80000.")
+    if verbose:
+        print(f"Es gilt t = {t} und damit Faktor = {(1 - (sigma * (alpha_2 ** t)))}")
+
+    x = x + ((alpha_2 ** t) * delta_x)
+    a = a + ((alpha_2 ** t) * delta_a)
+    mu = (1 - (sigma * (alpha_2 ** t))) * mu
+    if b is not None:
+        b = b + ((alpha_2 ** t) * delta_b)
+        return x, b, a, mu
+
+    return x, a, mu
 
 # ----------------------------------------------------------------------------------------------------------------------------- #
 """ Ab hier die Methoden die für die Implementierung des Algorithmus für LCPs benutzt wurden und nicht mehr verwendet werden. """
@@ -219,6 +393,8 @@ def linear_equation_formulate(
 
     return lhs, rhs
 
+
+
 def linear_equation_factorize(
     lhs: Union[np.ndarray, spa.csc_matrix],
     overwritelhs: bool = False,
@@ -289,103 +465,3 @@ def linear_equation_solve(
 
     return x
 
-def predictor_step(
-    x: np.ndarray,
-    y: np.ndarray,
-    delta_x: np.ndarray,
-    delta_y: np.ndarray,
-    mu: float,
-    alpha_1: float,
-    beta: float,
-    acc: float,
-    verbose: bool = False,    
-)   -> Optional[np.ndarray]:
-    r"""Beschreibung
-
-    Beschreibung
-
-    Parameters
-    ----------
-    a: 
-        in der Regel aktueller x-Vektor in |R^n.
-    b:
-        in der Regel aktueller y-Vektor in |R^n.
-    mu:
-        Glättungsparameter.
-    arg:
-        Integer, der aussagt nach welchem Argument abgeleitet wird.
-    verbose: bool
-        Boolean Variable um eine Ausgabe sämtlicher Zwischenergebnisse zu erzeugen.
-    """
-    verbose = False
-    if verbose:
-        print(f"Starting predictor_step calculation...")
-
-    # if np.linalg.norm(np.minimum(x + delta_x,y + delta_y)) < acc:
-    if np.linalg.norm(big_phi(x + delta_x, y + delta_y, 0)) < acc:
-        step = 1
-        x = x + delta_x
-        y = y + delta_y
-    elif np.linalg.norm(big_phi(x + delta_x, y + delta_y, mu, verbose=verbose)) > beta * mu:
-        step = 0
-    else:
-        step = 2
-        s = 1
-        while np.linalg.norm(big_phi(x + delta_x, y + delta_y, (alpha_1 ** s) * mu, verbose=verbose)) <= (alpha_1 ** s) * beta * mu:
-            if verbose:
-                print(f"erhöhe s um 1, {alpha_1 ** s} noch zu groß")
-            s += 1
-        x = x + delta_x
-        y = y + delta_y
-        mu = (alpha_1 ** (s-1)) * mu
-        if verbose:
-            print(f"{np.linalg.norm(big_phi(x + delta_x, y + delta_y, (alpha_1 ** (s-1)) * mu, verbose=verbose))} <= {(alpha_1 ** (s-1)) * beta * mu}")
-            print(f"{np.linalg.norm(big_phi(x + delta_x, y + delta_y, (alpha_1 ** (s-1)) * mu, verbose=verbose))} > {(alpha_1 ** s) * beta * mu}") 
-            print(f"daher eta_k = {alpha_1 ** (s-1)}")
-
-
-    return x, y, mu, step
-
-def corrector_step(
-    x: np.ndarray,
-    y: np.ndarray,
-    delta_x: np.ndarray,
-    delta_y: np.ndarray,
-    mu: float,
-    alpha_2: float,
-    beta: float,
-    sigma: float,
-    verbose: bool = False,    
-)   -> Optional[np.ndarray]:
-    r"""Beschreibung
-
-    Beschreibung
-
-    Parameters
-    ----------
-    a: 
-        in der Regel aktueller x-Vektor in |R^n.
-    b:
-        in der Regel aktueller y-Vektor in |R^n.
-    mu:
-        Glättungsparameter.
-    arg:
-        Integer, der aussagt nach welchem Argument abgeleitet wird.
-    verbose: bool
-        Boolean Variable um eine Ausgabe sämtlicher Zwischenergebnisse zu erzeugen.
-    """
-    verbose = False
-    if verbose:
-        print(f"Starting corrector_step calculation...")
-
-    t = 0
-    while np.linalg.norm(big_phi(x + ((alpha_2 ** t) * delta_x), y + ((alpha_2 ** t) * delta_y), (1 - (sigma * (alpha_2 ** t))) * mu, verbose=verbose)) > (1 - (sigma * (alpha_2 ** t))) * beta * mu:
-        if verbose:
-                print(f"erhöhe t um 1 auf {t+1}, {(1 - (sigma * (alpha_2 ** t)))} noch zu groß")
-        t += 1
-
-    x = x + ((alpha_2 ** t) * delta_x)
-    y = y + ((alpha_2 ** t) * delta_y)
-    mu = (1 - (sigma * (alpha_2 ** t))) * mu
-
-    return x, y, mu
